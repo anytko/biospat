b5e31eaa16583fbc134c4c84eef6bebba45ca9b3
Alivia Nytko <124749198+anytko@users.noreply.github.com>
2025-04-29 15:15:51 -0400

    Real deal (#6)
    * "Added functionality to identify and plot historic range edges"
    
    * "Added additional functionality to clip gbif polygons and assign clusters"
    
    * "Added required packages for new functionality"
    
    * "Installing the correct version of scikit-learn"
    
    * "Added a driver for opening .gpkg files"
    
    * "Switched to geojson from web for docs build"
    
    * "Updated all functions to use web geojson files"
    
    * "Removed fiona dependency"
    
    * "Reordered imports in init.py"
    
    * "Work around for request error"

--- biospat/__init__.py
+++ biospat/__init__.py
@@ -4,5 +4,8 @@ __author__ = """Alivia Nytko"""
 __email__ = "anytko@vols.utk.edu"
 __version__ = "0.0.2"
 
+from .biospat import *
+from .references_data import *
+from .stand_alone_functions import *
 from .mapping import *
 from .foliummap import *
